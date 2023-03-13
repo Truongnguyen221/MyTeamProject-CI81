cd5850defd7fbcc58bfb2981ff391a0ff566a199
minhquang231097 <minhquang231097@gmail.com>
2023-03-13 19:51:15 +0700

    first commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
+// 1. Import the extendTheme function
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { extendTheme, ChakraProvider } from '@chakra-ui/react'
 
+// 2. Extend the theme to include custom colors, fonts, etc
+const colors = {
+    brand: {
+        900: '#1a365d',
+        800: '#153e75',
+        700: '#2a69ac',
+    },
+}
+
+const theme = extendTheme({ colors })
+
+// 3. Pass the `theme` prop to the `ChakraProvider`
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <ChakraProvider theme={theme}>
+            <App />
+        </ChakraProvider>
+    </React.StrictMode>
 )
